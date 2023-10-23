e07245eb49b557aa042ace1a530db21a85317378
三哥 <373616885@qq.com>
2023-10-23 22:53:11 +0800

    vue

--- 前端/src/blog-app/vite.config.js
+++ 前端/src/blog-app/vite.config.js
@@ -4,4 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        // manualChunks: {
+        //   vue: ['vue']
+        // }
+        manualChunks(id) {
+          console.log(id)
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
