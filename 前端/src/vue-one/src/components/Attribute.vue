6c8c519827dacf276749ceaac18ef1884cdeade4
三哥 <373616885@qq.com>
2023-10-27 00:51:57 +0800

    vue

---
+++ 前端/src/vue-one/src/components/Attribute.vue
@@ -0,0 +1,45 @@
+<template>
+  <h3>{{ message }}</h3>
+  <!-- v-bind 绑定属性 -->
+  <div v-bind:id="dynamicId"></div>
+  <p>简写语法</p>
+  <div :id="dynamicId"></div>
+  <button :disabled="isButtonDisabled">Button</button>
+  <p>动态绑定多个值</p>
+  <div v-bind="objectOfAttrs">动态绑定多个值</div>
+  <button @click="changObj">Button</button>
+  <div v-bind="obj">修改id和class属性</div>
+</template>
+<script setup>
+import { ref } from 'vue'
+const message = ref('Attribute 绑定')
+const dynamicId = ref('container')
+const isButtonDisabled = true
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper'
+}
+const obj = ref({
+  id: 'container',
+  class: 'wrapper'
+})
+let num = 0
+function changObj() {
+  if (num % 2 == 0) {
+    obj.value.id = 'qinjp'
+    obj.value.class = 'qinjiepeng'
+  } else {
+    obj.value.id = 'container'
+    obj.value.class = 'wrapper'
+  }
+  num++
+}
+</script>
+<style scoped>
+#container {
+  font-size: 20px;
+}
+.wrapper {
+  color: red;
+}
+</style>
---
+++ 前端/src/vue-one/src/components/String.vue
@@ -0,0 +1,7 @@
+<template>
+  <h3>{{ str }}</h3>
+</template>
+<script setup>
+import { ref } from 'vue'
+const str = ref('文本插值')
+</script>
