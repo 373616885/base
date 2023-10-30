b4a40f6b64c26c0ed77e356ccd719fc9f958b561
三哥 <373616885@qq.com>
2023-10-31 00:31:35 +0800

    vue

---
+++ 前端/src/vue-router/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello App!</h1>
+    <!--使用 router-link 组件进行导航 -->
+    <!--通过传递 `to` 来指定链接 -->
+    <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+    <ol>
+      <li><router-link to="/">Go to Home</router-link></li>
+      <li><router-link to="/about">Go to About</router-link></li>
+    </ol>
+    <hr />
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
