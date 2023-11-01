36faee8f98409f42f282c77e7128da462515604a
三哥 <373616885@qq.com>
2023-11-02 00:41:31 +0800

    vue

---
+++ 前端/src/vue-filters/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>{{ msg }}</h3>
+
+  <FilterComponent />
+</template>
+
+<script setup>
+import FilterComponent from './components/FilterComponent.vue'
+const msg = '全局过滤器'
+</script>
